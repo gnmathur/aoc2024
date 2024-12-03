3a020a2a0d0b733ea1ee654f9469523aa0e96c42
Gaurav Mathur <gaurav.mathur@ymail.com>
2024-12-03 00:33:23 -0800

    Cleanup

--- src/main/java/dev/gmathur/Util.java
+++ src/main/java/dev/gmathur/Util.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Util {
-    public record AocResult<A, B>(A part1, B part2) {
-    }
+    public record AocResult<A, B>(A part1, B part2) { }
 
     public static <T> List<T> listExcludingElementAtI(List<T> list, int index) {
         List<T> result = new ArrayList<>(list);
-        result.remove(index); // Removes the element at the specified index
+        result.remove(index);
         return result;
     }
 }
