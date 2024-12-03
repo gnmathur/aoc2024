a9bb5a45606007c705f1556737364ba970449e99
Gaurav Mathur <gaurav.mathur@ymail.com>
2024-12-03 00:30:58 -0800

    Cleaned up d3

--- src/main/java/dev/gmathur/Day3MullItOver.java
+++ src/main/java/dev/gmathur/Day3MullItOver.java
@@ -5,35 +5,25 @@ import dev.gmathur.Util.AocResult;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Day3MullItOver {
-    private static long day2(String buffer) {
-        var regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)";
-        var matcher = Pattern.compile(regex).matcher(buffer);
-        var domul = new boolean[]{true};
+    private static long solver(final String buffer, final Boolean ignore) {
+        final var regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)";
+        final var matcher = Pattern.compile(regex).matcher(buffer);
+        final var domul = new boolean[]{true};
 
         return matcher.results()
-                .mapToLong(m -> switch (m.group()) {
+                .mapToLong(mr -> switch (mr.group()) {
                     case "do()" -> { domul[0] = true; yield 0; }
                     case "don't()" -> { domul[0] = false; yield 0; }
-                    default -> domul[0] ? Long.parseLong(m.group(1)) * Long.parseLong(m.group(2)) : 0;
+                    default -> (ignore || domul[0]) ? Long.parseLong(mr.group(1)) * Long.parseLong(mr.group(2)) : 0;
                 })
                 .sum();
     }
 
-    private static long day1(String buffer) {
-        String regex = "mul\\((\\d{1,3}),(\\d{1,3})\\)";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(buffer);
-
-        return matcher.results()
-                .mapToLong(matchResult ->
-                        Long.parseLong(matchResult.group(1)) * Long.parseLong(matchResult.group(2)))
-                .sum();
-    }
+    private static long day2(final String buffer) { return solver(buffer, false); }
+    private static long day1(final String buffer) { return solver(buffer, true); }
 
     public static AocResult<Long, Long> solve(){
         try {
