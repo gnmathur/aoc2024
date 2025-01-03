1f06987d6aacfb2fc12e3ce64c84070fe5779716
Gaurav Mathur <gaurav.mathur@ymail.com>
2025-01-03 01:38:03 -0800

    Completed day 7 part 1

--- src/main/java/dev/gmathur/Day7BridgeRepair.java
+++ src/main/java/dev/gmathur/Day7BridgeRepair.java
@@ -4,24 +4,27 @@ import dev.gmathur.Util.Pair;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class Day7BridgeRepair {
-    private record SolutionInput(List<Pair<Long, List<Long>>> input) {}
+    private record SolutionInput(List<Pair<BigInteger, List<BigInteger>>> input) {}
 
     private static SolutionInput readFileFromResources(final String fileName) {
-        List<Pair<Long, List<Long>>> input = new ArrayList<>();
+        List<Pair<BigInteger, List<BigInteger>>> input = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(
                 Objects.requireNonNull(Day7BridgeRepair.class.getClassLoader().getResourceAsStream(fileName))))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 String[] tokens = line.split(": ");
-                long target = Long.parseLong(tokens[0]);
-                List<Long> operands = Arrays.stream(tokens[1].split(" ")).map(Long::parseLong).toList();
+                BigInteger target = new BigInteger(tokens[0]);
+                List<BigInteger> operands = Arrays.stream(tokens[1].split(" "))
+                        .map(BigInteger::new)
+                        .toList();
                 var p = new Pair<>(target, operands);
                 input.add(p);
             }
@@ -31,29 +34,32 @@ public class Day7BridgeRepair {
 
         return new SolutionInput(input);
     }
-    // a b c
 
-    private static boolean solvePart1(long target, List<Long> operands, int idx, long result) {
-        if (idx == operands.size()) {
-            return result == target;
-        }
-        if (result > target) {
-            return false;
-        }
+    /**
+     * At each index, we have two choices: add or multiply the current operand to the result. We recursively try both
+     * choices and return true if we find a solution at the end of the operands list.
+     *
+     * @param target The target value to reach
+     * @param operands The list of operands
+     * @param idx The current index in the operands list
+     * @param result The current result
+     *
+     * @return True if we find a solution, false otherwise
+     */
+    private static boolean solvePart1(final BigInteger target, final List<BigInteger> operands, final int idx,
+                                      final BigInteger result) {
+        if (idx == operands.size()) { return result.equals(target); }
+        if (result.compareTo(target) > 0) { return false; }
 
-        return solvePart1(target, operands, idx + 1, result + (operands.get(idx))) ||
-                solvePart1(target, operands, idx + 1, result * (operands.get(idx)));
+        return solvePart1(target, operands, idx + 1, result.add(operands.get(idx))) ||
+                solvePart1(target, operands, idx + 1, result.multiply(operands.get(idx)));
     }
 
-    public static int part1(String fileName) {
-        SolutionInput input = readFileFromResources(fileName);
-        int result = 0;
-        for (Pair<Long, List<Long>> p : input.input) {
-            if (solvePart1(p.first(), p.second(), 0, 0)) {
-                result += p.first();
-            }
-        }
-        return result;
+    public static BigInteger part1(String fileName) {
+        final SolutionInput input = readFileFromResources(fileName);
+        return input.input.stream()
+                .filter(p -> solvePart1(p.first(), p.second(), 0, BigInteger.ZERO))
+                .map(Pair::first)
+                .reduce(BigInteger.ZERO, BigInteger::add);
     }
-
 }
--- src/test/java/dev/gmathur/Day7BridgeRepairTest.java
+++ src/test/java/dev/gmathur/Day7BridgeRepairTest.java
@@ -2,19 +2,22 @@ package dev.gmathur;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day7BridgeRepairTest {
     @Test
     void testTestInputPart1() {
         var r = Day7BridgeRepair.part1("day7/d7_test.input");
-        assertEquals(3749, r);
+        assertEquals(BigInteger.valueOf(3749), r);
     }
 
     @Test
     void testPuzzleInputPart1() {
         var r = Day7BridgeRepair.part1("day7/d7.input");
-        System.out.println(r);
+        // compare to 8401132154762
+        assertEquals(BigInteger.valueOf(8401132154762L), r);
     }
 
 }
