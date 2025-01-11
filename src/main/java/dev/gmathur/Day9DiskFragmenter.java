9fd289c3816a61ed839c63d9e32f8507bb52a644
Gaurav Mathur <gaurav.mathur@ymail.com>
2025-01-10 19:49:59 -0800

    Solved day 9 part 2

--- src/main/java/dev/gmathur/Day9DiskFragmenter.java
+++ src/main/java/dev/gmathur/Day9DiskFragmenter.java
@@ -4,51 +4,130 @@ import java.io.*;
 import java.util.*;
 import java.util.stream.IntStream;
 
+
+/**
+ * Solution for Day 9 of Advent of Code 2024 - Disk Fragmenter
+ * Problem description: <a href="https://adventofcode.com/2024/day/9">...</a>
+ * <p>
+ * Part 1
+ * 00...111...2...333.44.5555.6666.777.888899
+ *
+ */
 public class Day9DiskFragmenter {
-    private record Block(Optional<Long> id) {}
-    private record SolutionInput(List<Block> physicalDisk) {
-    }
+    private record SingleBlock(Long blockId, Optional<Long> fileId) {}
+    private record SolutionInput(List<SingleBlock> physicalDisk, List<List<SingleBlock>> files) {}
 
     private SolutionInput parse(String filename) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(
                 Day9DiskFragmenter.class.getClassLoader().getResourceAsStream(filename))))) {
-            final List<Block> physicalDisk = new ArrayList<>();
+            final List<SingleBlock> physicalDisk = new ArrayList<>();
+            final List<List<SingleBlock>> files = new ArrayList<>();
+            final List<List<SingleBlock>> freeSpaces = new ArrayList<>();
+
             final String line = reader.readLine();
             long ident = 0L;
+            long blockId = 0L;
 
             for (int diskMapIdx = 0; diskMapIdx <= line.length()-1; diskMapIdx +=2) {
-                var block_num = Long.parseLong(line.substring(diskMapIdx, diskMapIdx+1));
-                long freeNum = (diskMapIdx + 1 < line.length()) ? Long.parseLong(line.substring(diskMapIdx + 1, diskMapIdx + 2)) : 0;
-                physicalDisk.addAll(Collections.nCopies((int) block_num, new Block(Optional.of(ident))));
-                physicalDisk.addAll(Collections.nCopies((int) freeNum, new Block(Optional.empty())));
+                int block_num = Integer.parseInt(line.substring(diskMapIdx, diskMapIdx+1));
+                int freeNum = (diskMapIdx + 1 < line.length()) ? Integer.parseInt(line.substring(diskMapIdx + 1, diskMapIdx + 2)) : 0;
+
+                List<SingleBlock> fileBlocks = new ArrayList<>();
+                List<SingleBlock> freeBlocks = new ArrayList<>();
+
+                for (int i = 0; i < block_num; i++) { fileBlocks.add(new SingleBlock(blockId++, Optional.of(ident))); }
+                for (int i = 0; i < freeNum; i++) { freeBlocks.add(new SingleBlock(blockId++, Optional.empty())); }
+                physicalDisk.addAll(fileBlocks);
+                physicalDisk.addAll(freeBlocks);
+
+                files.add(fileBlocks);
+
                 ident += 1;
             }
-            return new SolutionInput(physicalDisk);
-
+            return new SolutionInput(physicalDisk, files);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
     }
 
-    private static int findNextFree(List<Block> physicalDisk, int startIdx) {
+    private static int findNextFree(List<SingleBlock> physicalDisk, int startIdx) {
         for (int i = startIdx; i < physicalDisk.size(); i++) {
-            if (physicalDisk.get(i).id().isEmpty()) { return i; }
+            if (physicalDisk.get(i).fileId().isEmpty()) { return i; }
         }
         return -1;
     }
 
-    private static int findNextNonFreeFromEnd(List<Block> physicalDisk, int startIdx) {
+    private static int findNextNonFreeFromEnd(List<SingleBlock> physicalDisk, int startIdx) {
         for (int i = startIdx; i >= 0; i--) {
-            if (physicalDisk.get(i).id().isPresent()) { return i; }
+            if (physicalDisk.get(i).fileId().isPresent()) { return i; }
         }
         return -1;
     }
 
-    private static void printDisk(List<Block> physicalDisk) {
-        for (Block block : physicalDisk) {
-            System.out.print(block.id().isEmpty() ? "." : block.id().get());
+    private static void printDisk(List<SingleBlock> physicalDisk) {
+        for (SingleBlock singleBlock : physicalDisk) {
+            System.out.print(singleBlock.fileId().isEmpty() ? "." : String.format("%s", singleBlock.fileId().get()));
         }
         System.out.println();
+    }
+
+    private static int findFirstContiguousFreeSpace(List<SingleBlock> physicalDisk, int size, long limit) {
+        int count = 0;
+        for (int i = 0; i < physicalDisk.size() && physicalDisk.get(i).blockId < limit; i++) {
+            if (physicalDisk.get(i).fileId().isEmpty()) {
+                count += 1;
+                if (count == size) { return i - size + 1; }
+            } else {
+                count = 0;
+            }
+        }
+        return -1;
+    }
+
+    private void moveFile(List<SingleBlock> physicalDisk, List<SingleBlock> fileToMove, int writeIdx) {
+        for (int i = 0; i < fileToMove.size(); i++) {
+            SingleBlock w = physicalDisk.get(writeIdx + i);
+            SingleBlock r = fileToMove.get(i);
+
+            // Swap the contents of the read and write blocks
+            SingleBlock newBlock = new SingleBlock(w.blockId(), r.fileId());
+            physicalDisk.set(writeIdx + i, newBlock);
+
+            newBlock = new SingleBlock(r.blockId(), Optional.empty());
+            physicalDisk.set(r.blockId().intValue(), newBlock);
+        }
+    }
+
+    private static long calculateChecksum(List<SingleBlock> physicalDisk) {
+        return IntStream.range(0, physicalDisk.size())
+                .filter(i -> physicalDisk.get(i).fileId().isPresent())
+                .mapToLong(i -> i * physicalDisk.get(i).fileId().get())
+                .sum();
+    }
+
+    public long part2(String fileName) {
+        final var solutionInput = parse(fileName);
+        final var physicalDisk = solutionInput.physicalDisk;
+        final var files = solutionInput.files;
+
+        int fileIdx = files.size()-1;
+        List<SingleBlock> fileToMove = files.get(fileIdx);
+        int writeIdx = findFirstContiguousFreeSpace(physicalDisk, fileToMove.size(), fileToMove.getFirst().blockId);
+
+        while (true) {
+            if (writeIdx != -1) {
+                moveFile(physicalDisk, fileToMove, writeIdx);
+            }
+
+            fileIdx -= 1;
+            if (fileIdx < 0) {
+                break;
+            }
+            fileToMove = files.get(fileIdx);
+            writeIdx = findFirstContiguousFreeSpace(physicalDisk, fileToMove.size(), fileToMove.getFirst().blockId);
+        }
+
+        return calculateChecksum(physicalDisk);
     }
 
     public long part1(String fileName) {
@@ -65,11 +144,7 @@ public class Day9DiskFragmenter {
             readIdx = findNextNonFreeFromEnd(physicalDisk, readIdx-1);
         }
 
-        // Go through the physical disk and multiply the index of the block with the block id, if it is not empty
-        return IntStream.range(0, physicalDisk.size())
-                .filter(i -> physicalDisk.get(i).id().isPresent())
-                .mapToLong(i -> i * physicalDisk.get(i).id().get())
-                .sum();
+        // Go through the physical disk and multiply the index of the block with the block fileId, if it is not empty
+        return calculateChecksum(physicalDisk);
     }
-
 }
--- src/test/java/dev/gmathur/Day9DiskFragmenterTest.java
+++ src/test/java/dev/gmathur/Day9DiskFragmenterTest.java
@@ -6,17 +6,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Day9DiskFragmenterTest {
     @Test
-    void testPart1() {
+    void testPart1WithTestInput() {
         var day9DiskFragmenter = new Day9DiskFragmenter();
         var r = day9DiskFragmenter.part1("day9/d9_test.input");
         assertEquals(1928, r);
     }
 
     @Test
-    void testPart2() {
-        var day9DiskFragmenter = new Day9DiskFragmenter();
-        var r = day9DiskFragmenter.part1("day9/d9.input");
-        assertEquals(6398252054886L, r);
+    void testPart1WithProblemInput() {
+        Util.runTimed(() -> {
+            var day9DiskFragmenter = new Day9DiskFragmenter();
+            var r = day9DiskFragmenter.part1("day9/d9.input");
+            assertEquals(6398252054886L, r);
+        });
     }
 
+    @Test
+    void testPart2WithTestInput() {
+        var day9DiskFragmenter = new Day9DiskFragmenter();
+        var r = day9DiskFragmenter.part2("day9/d9_test.input");
+        assertEquals(2858, r);
+    }
+
+    @Test
+    void testPart2WithProblemInput() {
+        Util.runTimed(() -> {
+            var day9DiskFragmenter = new Day9DiskFragmenter();
+            var r = day9DiskFragmenter.part2("day9/d9.input");
+            assertEquals(6415666220005L, r);
+        });
+    }
 }
