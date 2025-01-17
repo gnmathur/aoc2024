190d91efc7cf86a2ca50cb3c97459ced68da704e
Gaurav Mathur <gaurav.mathur@ymail.com>
2025-01-16 20:21:55 -0800

    Day 12 parts 1 and 2

--- src/main/java/dev/gmathur/Day12GardenGroups.java
+++ src/main/java/dev/gmathur/Day12GardenGroups.java
@@ -1,18 +1,36 @@
 package dev.gmathur;
 
-import dev.gmathur.Util.FileLineIterator;
 import dev.gmathur.Util.Pair;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
+/**
+ * --- Day 12: Garden Groups ---
+ * https://adventofcode.com/2024/day/12
+ *
+ * Notes:
+ * 1. Both parts use a classic DFS to find the distinct regions in the garden.
+ * 2. Part 1 can use the information returned by the DFS to calculate the number of fences needed to enclose each region.
+ * 3. For Part 2, the intuition to count the corners was relatively straightforward. However, the implementation was
+ * very hard to get right. After a lot of trial and error, I think I have managed to come up with the canonical set
+ * of criterion. Implementation-wise it's actually possible to make it more concise, but I think the current
+ * implementation * is more readable and makes the logic more clear.
+ *
+ * Time Complexity of Part 1: O(R * C) where R is the number of rows and C is the number of columns
+ * Time Complexity of Part 2: O(R * C) where R is the number of rows and C is the number of columns
+ *
+ * Runtime on MacBook Pro M3:
+ *
+ * Part 1: 21ms
+ * Part 2: 16ms
+ */
 public class Day12GardenGroups {
-    private record SolutionInput(char[][] garden, int R, int C) {}
+    public record SolutionInput(char[][] garden, int R, int C) {}
 
-    private static SolutionInput parse(String fileName) {
+    public static SolutionInput parse(String fileName) {
         try (var reader = new BufferedReader(new InputStreamReader(
                 Objects.requireNonNull(Day09DiskFragmenter.class.getClassLoader().getResourceAsStream(fileName))))) {
             var garden = reader.lines()
@@ -28,11 +46,27 @@ public class Day12GardenGroups {
         }
     }
 
-    private static void part1Solver(char[][] garden, int r, int c, int R, int C, Set<Pair<Integer, Integer>> discovered,
-                                    long[] fenceAndCount, char region) {
-        var coor = new Pair<>(r, c);
+    /**
+     * Populates the region of the garden starting at (r, c) with the region character region.
+     *
+     * @param gardenPlots All the garden plots
+     * @param r We need to start completing a region starting at this row
+     * @param c We need to start completing a region starting at this column
+     * @param R Number of rows in the garden
+     * @param C Number of columns in the garden
+     * @param discovered DFS discovered set
+     * @param fenceAndCount An array of size 2, where the first element is the number of fences needed to complete the
+     *                      region, and the second element is the number of cells in the region
+     * @param region The region character
+     * @param regionGardens A list of all the cells in the region. Populated by this method
+     */
+    private static void populateRegion(final char[][] gardenPlots, final int r, final int c, final int R, final int C,
+                                       final Set<Pair<Integer, Integer>> discovered, final long[] fenceAndCount,
+                                       final char region, final List<Pair<Integer, Integer>> regionGardens) {
+        var coordinate = new Pair<>(r, c);
 
-        discovered.add(coor);
+        discovered.add(coordinate);
+        regionGardens.add(coordinate);
 
         int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
@@ -41,17 +75,68 @@ public class Day12GardenGroups {
             int newC = c + dir[1];
             var newCoor = new Pair<>(newR, newC);
 
-            if (!(newR >= 0 && newR < R && newC >= 0 && newC < C && garden[newR][newC] == region)) {
+            if (!(newR >= 0 && newR < R && newC >= 0 && newC < C && gardenPlots[newR][newC] == region)) {
                 fenceAndCount[0] += 1;
             } else if (!discovered.contains(newCoor)) {
                 fenceAndCount[1] += 1;
-                part1Solver(garden, newR, newC, R, C, discovered, fenceAndCount, region);
+                populateRegion(gardenPlots, newR, newC, R, C, discovered, fenceAndCount, region, regionGardens);
             }
         }
     }
 
-    public static long part1(String fileName) {
-        var si = parse(fileName);
+    public static int countCorners(List<Pair<Integer, Integer>> regionGardens, char[][] grid, int rows, int cols,
+                                   char region) {
+        int corners = 0;
+        for (Pair<Integer, Integer> coor : regionGardens) {
+            int r = coor.first();
+            int c = coor.second();
+
+            // if the top and left cells are region cells, but the top let cell is not, then this is a corner
+            // ... | X | R | ...
+            // ... | R | R | ...
+            if (r > 0 && c > 0 && grid[r - 1][c] == region && grid[r][c - 1] == region && grid[r - 1][c - 1] != region) {
+                corners++;
+            }
+            // if the top and right cells are region cells, but the top right cell is not, then this is a corner
+            // ... | R | X | ...
+            // ... | R | R | ...
+            if (r > 0 && c < cols - 1 && grid[r - 1][c] == region && grid[r][c + 1] == region && grid[r - 1][c + 1] != region) {
+                corners++;
+            }
+            // if the bottom and left cells are region cells, but the bottom left cell is not, then this is a corner
+            // ... | R | R | ...
+            // ... | X | R | ...
+            if (r < rows - 1 && c > 0 && grid[r + 1][c] == region && grid[r][c - 1] == region && grid[r + 1][c - 1] != region) {
+                corners++;
+            }
+            // if the bottom and right cells are region cells, but the bottom right cell is not, then this is a corner
+            // ... | R | R | ...
+            // ... | R | X | ...
+            if (r < rows - 1 && c < cols - 1 && grid[r + 1][c] == region && grid[r][c + 1] == region && grid[r + 1][c + 1] != region) {
+                corners++;
+            }
+
+            // if the top cell is out of bounds or not region, and left cell is out of bounds or not region
+            if ((r == 0 || grid[r - 1][c] != region) && (c == 0 || grid[r][c - 1] != region)) {
+                corners++;
+            }
+            // if the top cell is out of bounds or not region, and right cell is out of bounds or not region
+            if ((r == 0 || grid[r - 1][c] != region) && (c == cols - 1 || grid[r][c + 1] != region)) {
+                corners++;
+            }
+            // if the bottom cell is out of bounds or not region, and left cell is out of bounds or not region
+            if ((r == rows - 1 || grid[r + 1][c] != region) && (c == 0 || grid[r][c - 1] != region)) {
+                corners++;
+            }
+            // if the bottom cell is out of bounds or not region, and right cell is out of bounds or not region
+            if ((r == rows - 1 || grid[r + 1][c] != region) && (c == cols - 1 || grid[r][c + 1] != region)) {
+                corners++;
+            }
+        }
+        return corners;
+    }
+
+    public static long part2(SolutionInput si) {
         var discovered = new HashSet<Pair<Integer, Integer>>();
         var totalPrices = 0L;
 
@@ -59,7 +144,30 @@ public class Day12GardenGroups {
             for (int c = 0; c < si.C; c++) {
                 if (!discovered.contains(new Pair<>(r, c))) {
                     long[] fenceAndCount = new long[]{0, 1};
-                    part1Solver(si.garden, r, c, si.R, si.C, discovered, fenceAndCount, si.garden[r][c]);
+                    List<Pair<Integer, Integer>> regionGardens = new ArrayList<>();
+
+                    populateRegion(si.garden, r, c, si.R, si.C, discovered, fenceAndCount, si.garden[r][c], regionGardens);
+                    // We'll ignore the fence count and just count the corners. The number of corners is the same as
+                    // the number of sides of the region
+                    int corners = countCorners(regionGardens, si.garden, si.R, si.C, si.garden[r][c]);
+                    totalPrices += corners * fenceAndCount[1];
+                }
+            }
+        }
+        return totalPrices;
+    }
+
+    public static long part1(SolutionInput si) {
+        var discovered = new HashSet<Pair<Integer, Integer>>();
+        var totalPrices = 0L;
+
+        for (int r = 0; r < si.R; r++) {
+            for (int c = 0; c < si.C; c++) {
+                if (!discovered.contains(new Pair<>(r, c))) {
+                    long[] fenceAndCount = new long[]{0, 1};
+                    List<Pair<Integer, Integer>> regionGardens = new ArrayList<>();
+
+                    populateRegion(si.garden, r, c, si.R, si.C, discovered, fenceAndCount, si.garden[r][c], regionGardens);
                     totalPrices += fenceAndCount[0] * fenceAndCount[1];
                 }
             }
--- src/test/java/dev/gmathur/Day12GardenGroupsTest.java
+++ src/test/java/dev/gmathur/Day12GardenGroupsTest.java
@@ -1,24 +1,46 @@
 package dev.gmathur;
 
+import dev.gmathur.Day12GardenGroups.SolutionInput;
 import org.junit.jupiter.api.Test;
 
+import static dev.gmathur.Day12GardenGroups.parse;
 import static dev.gmathur.Util.runTimedWithLabel;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Day12GardenGroupsTest {
+    // Create solution input so that it can be used by all tests
+    private static final SolutionInput testSolutionInput = parse("day12/d12_test.input");
+    private static final SolutionInput puzzleSlutionInput = parse("day12/d12.input");
+
     @Test
     void testPart1WithTestInput() {
-        runTimedWithLabel("D12 Part 1", () -> {
-            var r = Day12GardenGroups.part1("day12/d12_test.input");
+        runTimedWithLabel("D12 Part 1 Test Input", () -> {
+            var r = Day12GardenGroups.part1(testSolutionInput);
             assertEquals(1930, r);
         });
     }
 
     @Test
     void testPart1WithProblemInput() {
-        runTimedWithLabel("D12 Part 2", () -> {
-            var r = Day12GardenGroups.part1("day12/d12.input");
+        runTimedWithLabel("D12 Part 1 Puzzle Input", () -> {
+            var r = Day12GardenGroups.part1(puzzleSlutionInput);
             assertEquals(1550156L, r);
+        });
+    }
+
+    @Test
+    void testPart2WithTestInput() {
+        runTimedWithLabel("D12 Part 2 Test Input", () -> {
+            var r = Day12GardenGroups.part2(testSolutionInput);
+            assertEquals(1206, r);
+        });
+    }
+
+    @Test
+    void testPart2WithProblemInput() {
+        runTimedWithLabel("D12 Part 2 Puzzle Input", () -> {
+            var r = Day12GardenGroups.part2(puzzleSlutionInput);
+            assertEquals(946084, r);
         });
     }
 }
