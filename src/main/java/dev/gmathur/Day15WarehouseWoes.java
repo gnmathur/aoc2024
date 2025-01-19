2dbd1274a78e533450171395a2bf3d5ac7029ad6
Gaurav Mathur <gaurav.mathur@ymail.com>
2025-01-19 11:48:09 -0800

    Refactored day 15 part1

--- src/main/java/dev/gmathur/Day15WarehouseWoes.java
+++ src/main/java/dev/gmathur/Day15WarehouseWoes.java
@@ -53,97 +53,98 @@ public class Day15WarehouseWoes {
         }
     }
 
-    private static ArrayDeque<Cell> cellsToTheLeftOf(Cell cell, char[][] grid, int R, int C) {
-        ArrayDeque<Cell> cells = new ArrayDeque<>();
-        for (int i = cell.c() - 1; i >= 0; i--) {
-            if (grid[cell.r()][i] != 'O') { break; }
-            cells.add(new Cell(cell.r(), i));
+    /**
+     * Find cells that are marked as 'O' in the grid, and are adjacent to the robot cell in the given direction. The direction
+     * to search in is specified by dr and dc (UP => (-1, 0), DOWN => (1, 0), LEFT => (0, -1), RIGHT => (0, 1)).
+     *
+     * @param robotLocation the cell the robot is in currently
+     * @param grid the grid to search in
+     * @param R the number of rows in the grid
+     * @param C the number of columns in the grid
+     * @param dr Row offset specifying the direction to search in
+     * @param dc Column offset specifying the direction to search in
+     *
+     * @return a deque of cells that are marked as 'O' and are adjacent to the robot cell in the given direction
+     */
+    private static ArrayDeque<Cell> cellsInDirection(final Cell robotLocation, final char[][] grid, final int R,
+                                                     final int C, final int dr, final int dc) {
+        final ArrayDeque<Cell> cells = new ArrayDeque<>();
+        int r = robotLocation.r() + dr;
+        int c = robotLocation.c() + dc;
+
+        while (r >= 0 && r < R && c >= 0 && c < C && grid[r][c] == 'O') {
+            cells.add(new Cell(r, c));
+            r += dr; c += dc;
         }
+
         return cells;
+    }
+
+    private static ArrayDeque<Cell> cellsToTheLeftOf(Cell cell, char[][] grid, int R, int C) {
+        return cellsInDirection(cell, grid, R, C, 0, -1);
     }
 
     private static ArrayDeque<Cell> cellsToTheRightOf(Cell cell, char[][] grid, int R, int C) {
-        ArrayDeque<Cell> cells = new ArrayDeque<>();
-        for (int i = cell.c() + 1; i < C; i++) {
-            if (grid[cell.r()][i] != 'O') { break; }
-            cells.add(new Cell(cell.r(), i));
-        }
-        return cells;
+        return cellsInDirection(cell, grid, R, C, 0, 1);
     }
 
     private static ArrayDeque<Cell> cellsAbove(Cell cell, char[][] grid, int R, int C) {
-        ArrayDeque<Cell> cells = new ArrayDeque<>();
-        for (int i = cell.r() - 1; i >= 0; i--) {
-            if (grid[i][cell.c()] != 'O') { break; }
-            cells.add(new Cell(i, cell.c()));
-        }
-        return cells;
+        return cellsInDirection(cell, grid, R, C, -1, 0);
     }
 
     private static ArrayDeque<Cell> cellsBelow(Cell cell, char[][] grid, int R, int C) {
-        ArrayDeque<Cell> cells = new ArrayDeque<>();
-        for (int i = cell.r() + 1; i < R; i++) {
-            if (grid[i][cell.c()] != 'O') { break; }
-            cells.add(new Cell(i, cell.c()));
-        }
-        return cells;
+        return cellsInDirection(cell, grid, R, C, 1, 0);
     }
 
-    private static Cell moveCellsLeft(ArrayDeque<Cell> cells, char[][] grid, Cell robot) {
-        // If the last cell in cells list is at the left edge of the grid, or is to the right of a a cell with #, then
-        // we can't move the cells to the left
-        if (cells.getLast().c() == 0) { return robot; }
-        if (grid[cells.getLast().r()][cells.getLast().c() - 1] == '#') { return robot; }
-        // Shift all cells in the list to the left on the grid
+    /**
+     * Move the cells in the given direction. The direction is specified by dr and dc (UP => (-1, 0), DOWN => (1, 0),
+     * LEFT => (0, -1), RIGHT => (0, 1)). Note that there is a need to reverse the order of the cells in the deque
+     * because the leading cell is the one that moves first, and the trailing cell is the one that moves last.
+     *
+     * @param cells the cells to move
+     * @param grid the grid to move the cells in
+     * @param robot the cell the robot is in currently
+     * @param R the number of rows in the grid
+     * @param C the number of columns in the grid
+     * @param dr Row offset specifying the direction to move in
+     * @param dc Column offset specifying the direction to move in
+     *
+     * @return the new location of the robot after moving the cells
+     */
+    private static Cell moveCells(final ArrayDeque<Cell> cells, final char[][] grid, final Cell robot, int R, int C,
+                                  int dr, int dc) {
+        final Cell leadingCell = cells.getLast();
+        int newRow = leadingCell.r() + dr;
+        int newCol = leadingCell.c() + dc;
+
+        if (newRow < 0 || newRow >= R || newCol < 0 || newCol >= C) { return robot; }
+        if (grid[newRow][newCol] == '#') { return robot; }
+
         for (Cell cell : cells.reversed()) {
-            grid[cell.r()][cell.c() - 1] = grid[cell.r()][cell.c()];
+            grid[cell.r() + dr][cell.c() + dc] = grid[cell.r()][cell.c()];
             grid[cell.r()][cell.c()] = '.';
         }
 
-        return new Cell(robot.r(), robot.c() - 1);
+        return new Cell(robot.r() + dr, robot.c() + dc);
     }
 
-    private static Cell moveCellsRight(ArrayDeque<Cell> cells, char[][] grid, Cell robot) {
-        // If the last cell in cells list is at the right edge of the grid, or is to the left of a a cell with #, then
-        // we can't move the cells to the right
-        if (cells.getLast().c() == grid[0].length - 1) { return robot; }
-        if (grid[cells.getLast().r()][cells.getLast().c() + 1] == '#') { return robot; }
-        // Shift all cells in the list to the right on the grid
-        for (Cell cell : cells.reversed()) {
-            grid[cell.r()][cell.c() + 1] = grid[cell.r()][cell.c()];
-            grid[cell.r()][cell.c()] = '.';
-        }
-
-        return new Cell(robot.r(), robot.c() + 1);
+    private static Cell moveCellsLeft(final ArrayDeque<Cell> cells, final char[][] grid, final int R, final int C,
+                                      final Cell robot) {
+        return moveCells(cells, grid, robot, R, C, 0, -1);
+    }
+    private static Cell moveCellsRight(final ArrayDeque<Cell> cells, final char[][] grid, final int R, final int C,
+                                       final Cell robot) {
+        return moveCells(cells, grid, robot, R, C, 0, 1);
+    }
+    private static Cell moveCellsUp(final ArrayDeque<Cell> cells, final char[][] grid, final int R, final int C,
+                                    final Cell robot) {
+        return moveCells(cells, grid, robot, R, C, -1, 0);
+    }
+    private static Cell moveCellsDown(final ArrayDeque<Cell> cells, final char[][] grid, final int R, final int C,
+                                      final Cell robot) {
+        return moveCells(cells, grid, robot, R, C, 1, 0);
     }
 
-    private static Cell moveCellsUp(ArrayDeque<Cell> cells, char[][] grid, Cell robot) {
-        // If the last cell in cells list is at the top edge of the grid, or is below a a cell with #, then
-        // we can't move the cells up
-        if (cells.getLast().r() == 0) { return robot; }
-        if (grid[cells.getLast().r() - 1][cells.getLast().c()] == '#') { return robot; }
-        // Shift all cells in the list up on the grid
-        for (Cell cell : cells.reversed()) {
-            grid[cell.r() - 1][cell.c()] = grid[cell.r()][cell.c()];
-            grid[cell.r()][cell.c()] = '.';
-        }
-
-        return new Cell(robot.r() - 1, robot.c());
-    }
-
-    private static Cell moveCellsDown(ArrayDeque<Cell> cells, char[][] grid, Cell robot) {
-        // If the last cell in cells list is at the bottom edge of the grid, or is above a a cell with #, then
-        // we can't move the cells down
-        if (cells.getLast().r() == grid.length - 1) { return robot; }
-        if (grid[cells.getLast().r() + 1][cells.getLast().c()] == '#') { return robot; }
-        // Shift all cells in the list down on the grid
-        for (Cell cell : cells.reversed()) {
-            grid[cell.r() + 1][cell.c()] = grid[cell.r()][cell.c()];
-            grid[cell.r()][cell.c()] = '.';
-        }
-
-        return new Cell(robot.r() + 1, robot.c());
-    }
 
     private static void printGrid(char[][] grid) {
         for (char[] row : grid) {
@@ -159,9 +160,7 @@ public class Day15WarehouseWoes {
         long sum = 0;
         for (int r = 0; r < grid.length; r++) {
             for (int c = 0; c < grid[r].length; c++) {
-                if (grid[r][c] == 'O') {
-                    sum += 100 * (long)(r+1) + (c+1);
-                }
+                sum += grid[r][c] == 'O' ? 100L * (r + 1) + (c + 1) : 0;
             }
         }
         return sum;
@@ -170,29 +169,30 @@ public class Day15WarehouseWoes {
     public static long part1(final String fileName) {
         SolutionInput input = parse(fileName);
         Cell start = input.start();
+        int R = input.grid().length;
+        int C = input.grid()[0].length;
 
         for (Character move : input.moves) {
-            //  <vv>^
             switch (move) {
                 case '<' -> {
-                    var cells = cellsToTheLeftOf(start, input.grid(), input.grid().length, input.grid()[0].length);
+                    var cells = cellsToTheLeftOf(start, input.grid(), R, C);
                     cells.addFirst(start);
-                    start = moveCellsLeft(cells, input.grid(), start);
+                    start = moveCellsLeft(cells, input.grid(), R, C, start);
                 }
                 case '>' -> {
-                    var cells = cellsToTheRightOf(start, input.grid(), input.grid().length, input.grid()[0].length);
+                    var cells = cellsToTheRightOf(start, input.grid(), R, C);
                     cells.addFirst(start);
-                    start = moveCellsRight(cells, input.grid(), start);
+                    start = moveCellsRight(cells, input.grid(), R, C, start);
                 }
                 case '^' -> {
-                    var cells = cellsAbove(start, input.grid(), input.grid().length, input.grid()[0].length);
+                    var cells = cellsAbove(start, input.grid(), R, C);
                     cells.addFirst(start);
-                    start = moveCellsUp(cells, input.grid(), start);
+                    start = moveCellsUp(cells, input.grid(), R, C, start);
                 }
                 case 'v' -> {
-                    var cells = cellsBelow(start, input.grid(), input.grid().length, input.grid()[0].length);
+                    var cells = cellsBelow(start, input.grid(), R, C);
                     cells.addFirst(start);
-                    start = moveCellsDown(cells, input.grid(), start);
+                    start = moveCellsDown(cells, input.grid(), R, C, start);
                 }
             }
         }
